6f1d54048ea5c7b36850e16cb0ac3a593e9874b8
mykhailo_l <rosmisha@gmail.com>
2025-04-03 10:22:37 +0300

    first attempt

---
+++ TestProject/DTO/PublishMessageRequestDTO.cs
@@ -0,0 +1,6 @@
+namespace TestProject.DTO;
+
+public sealed class PublishMessageRequestDTO
+{
+    public string Message { get; set; } = string.Empty;
+}
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,13 +1,27 @@
 namespace TestProject;
 
+using DTO;
+using LovgaSatellite;
+using LovgaSatellite.GrpcClientServices;
+using Microsoft.AspNetCore.Mvc;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        Extensions.InitChannel("localhost", 8080);
+
+        builder.Services.AddScoped<PublisherGrpcClient>();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapPost("/publish-message", async (PublisherGrpcClient client, [FromBody] PublishMessageRequestDTO message) =>
+        {
+            var result = await client.PublishMessage($"{DateTime.UtcNow} - {message.Message}", "bobr-topic");
+
+            return result;
+        });
 
         app.Run();
     }
